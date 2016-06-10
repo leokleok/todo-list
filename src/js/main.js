2c0e5ff2aa41454b82f18eef06737fad17227b7a
leok si <leoksi.koh@gmail.com>
2016-06-10 13:17:10 +0800

    fulfil add input text and list task under todo-now

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function(){
 
-  
+ $(".input-group-addon").click(function(){
+   console.log('button working')
+   var toAdd = $(".form-control").val()
+
+   $(".todo-now").append(
+     "<td>" + toAdd + "</td>" +
+     '<td class="text-right">' +
+     '<button class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i></button>' +
+     '<button class="btn btn-success"><i class="glyphicon glyphicon-ok"></i></button>' +
+     "</td>"
+   );
+ });
 });
