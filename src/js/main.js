22f41e3301bce205c4246424b14afdb1a3cdd553
leok si <leoksi.koh@gmail.com>
2016-06-10 14:23:40 +0800

    add if else statement for input text

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
 
  $(".input-group-addon").click(function(){
-   console.log('button working')
-   var toAdd = $(".form-control").val()
 
-   $(".todo-now").append(
-     "<td>" + toAdd + "</td>" +
-     '<td class="text-right">' +
-     '<button class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i></button>' +
-     '<button class="btn btn-success"><i class="glyphicon glyphicon-ok"></i></button>' +
-     "</td>"
-   );
+   if($(".form-control").val() !== '') {
+     var toAdd = $(".form-control").val()
+     $(".todo-now").append(
+       "<td>" + toAdd + "</td>" +
+       '<td class="text-right">' +
+       '<button class="btn btn-danger"><i class="glyphicon glyphicon-remove"></i></button>' +
+       '<button class="btn btn-success"><i class="glyphicon glyphicon-ok"></i></button>' +
+       "</td>"
+     );
+   }
+   else {
+        return false;
+   }
+
+ });
+
+ $(".btn btn-danger").on("click", function(event){
+
+      console.log('remove');
  });
 });
